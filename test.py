d7b9c2256e55e1bbe127103091750ad3a691d52c
chelvanai <chelvamchelvan@yahoo.com>
2020-11-15 14:49:02 +0530

    dev

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 import tensorflow as tf
+import librosa
+import numpy as np
+import math
 
-model = tf.keras.models.load_model('music_genres_model.h5')
+# model = tf.keras.models.load_model('music_genres_model.h5')
 
+signal, sample_rate = librosa.load('classical_test.wav', sr=22050)
 
+print(signal.shape)
 
+mfcc = librosa.feature.mfcc(signal, sample_rate, n_mfcc=13, n_fft=2048,
+                            hop_length=512)
+
+print(mfcc.shape)
